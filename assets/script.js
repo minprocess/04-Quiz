36946dbb76e3c5dbed3456a627d7a3995f40fdfc
Bill Pate <wtpate69@hotmail.com>
2021-03-28 10:44:21 -0400

    get local storage working

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
 var choicesList = document.querySelector("#choices-list");  // ul
 var questionText = document.querySelector("#question");
 var timeLeftText = document.querySelector("#time");
-var myBtn = document.querySelector("#my-button");
+var nextBtn = document.querySelector("#next-button");
 var correctAnsText = document.querySelector("#correct-ans");
 var yourAnsText = document.querySelector("#your-ans");
 var scoreText = document.querySelector("#score");
+var numCorrAnsSoText = document.querySelector("numCorrectAnsSoFar");
+var hofQuestionText = document.querySelector("hofQuestion");
+var recScoreBtn = document.querySelector.apply("record-score-button");
 var curQuest = 0;
 var stage = 0;
 var maxTimeGiven = 15;  // seconds
@@ -13,6 +16,7 @@ var countSetInterval = 0;
 //   0     initialize-listener for li, stage = 1
 //   1      Ask question, button says next, if last question, 
 
+var numCorrAnsSoFar;
 var score;
 var timeLeft;
 var waitFlag;
@@ -20,6 +24,7 @@ var choiceCount;
 var maxChoices = 7;
 var score = 0;
 var hasBeenClicked = false;
+var hofTable;  // Hall of Fame stored scores
 
 var questions = [
     {
@@ -80,7 +85,7 @@ function changeChoices() {
 }
 
 // Handler for click on Button
-function btnClick() {
+function btnClickNext() {
   console.log("In btnClick");
   if (stage == 0) {
     // Stage 1 - Waiting to start
@@ -89,7 +94,7 @@ function btnClick() {
     console.log("    choicesList ", choicesList);
     console.log("    questionText ", questionText);
     console.log("    timeLeftText ", timeLeftText);
-    console.log("    myBtn ", myBtn);
+    console.log("    nextBtn ", nextBtn);
     console.log("    correctAnsText ", correctAnsText);
     console.log("    yourAnsText ", yourAnsText);
     console.log("    scoreText ", scoreText);
@@ -97,10 +102,10 @@ function btnClick() {
     setAttributes();   // setAttributes of li elements
     changeChoices();
 
-    choicesList.addEventListener("click", myOnClick);   // listen for click on a choice
+    choicesList.addEventListener("click", onClickChoice);   // listen for click on a choice
     timeLeftText.text = maxTimeGiven;
     timeLeft = maxTimeGiven;
-    myBtn.textContent = "Next";
+    nextBtn.textContent = "Next";
     scoreText.textContent = "0/" + questions.length;
     timeLeftText.textContent = maxTimeGiven;
     // These appear below question and choices
@@ -108,6 +113,7 @@ function btnClick() {
     yourAnsText.textContent = "";
     countSetInterval++;
     hasBeenClicked = false;
+    recScoreBtn = true;
     x = setInterval(myOnTimer, 1000);
     console.log("btnClick stage 1, x", x);
   }
@@ -122,7 +128,7 @@ function btnClick() {
       changeChoices();
       timeLeftText.text = maxTimeGiven;
       timeLeft = maxTimeGiven;
-      myBtn.textContent = "Next";
+      nextBtn.textContent = "Next";
       yourAnsText.textContent = "Your answer: ";
       correctAnsText.textContent = "The correct answer: ";
       countSetInterval++;
@@ -146,13 +152,13 @@ function myOnTimer() {
   }
 }
 
-function incScore() {
-  score++;
-  scoreText.textContent = "Correct answers: " + score;
+function UpdateDashboard() {
+  scoreText.textContent = score;
+  
 }
 
 // This is handler for clicking one of the choices
-function myOnClick(e) {
+function onClickChoice(e) {
   var target = e.target; // Clicked element
   while (target && target.parentNode !== choicesList) {
     target = target.parentNode; // If the clicked element isn't a direct child
@@ -162,9 +168,9 @@ function myOnClick(e) {
   console.log("target.tagName: ", target.tagName);
   if (target.tagName === "LI") {
     var index = target.getAttribute("data-index");
-    console.log("myOnClick, index", index);
-    console.log("myOnClick, choiceCount", choiceCount);
-    console.log("myOnClick, choice", questions[curQuest].choices[index]);
+    console.log("onClickChoice, index", index);
+    console.log("onClickChoice, choiceCount", choiceCount);
+    console.log("onClickChoice, choice", questions[curQuest].choices[index]);
     hasBeenClicked = true;
     clearInterval(x);  // stop timer
     //var msg;
@@ -172,22 +178,72 @@ function myOnClick(e) {
       // will only be here if user clicked a blank choice. Don't allow that
       yourAnsText.textContent = "You clicked on a blank choice";
       correctAnsText.textContent = "The correct answer is " + questions[curQuest].answer + ". Better luck next time!";
+      score += 0;   // User does not receive any points
     }
     else if (index == choiceCount-1) {
       yourAnsText.textContent = "You clicked 'I don't know'";
       correctAnsText.textContent = "The correct answer is " + questions[curQuest].answer + ". Better luck next time!";
+      score += 0;   // User does not receive any points
     }
     else if (questions[curQuest].choices[index] == questions[curQuest].answer) {
       console.log("correct answer!");
-      yourAnsText.textContent = "You are right! Your answer is " + questions[curQuest].choices[index] + ". Good going!";
-      correctAnsText.textContent = "The correct answer is " + questions[curQuest].answer;
-      incScore();
+      yourAnsText.textContent = "Your answer is " + questions[curQuest].choices[index] + "which is correct! Good going!";
+      correctAnsText.textContent = "";
+      score += timeLeft;
+      numCorrectAnswersSoFar += 1;
+      UpdateDashboard();
     }
     else {
       yourAnsText.textContent = "Sorry, wrong answer! Your answer is " + questions[curQuest].choices[index];
       correctAnsText.textContent = "The correct answer is " + questions[curQuest].answer + ". Better luck next time!";
     }
-    myBtn.textContent = "Next";
-  }   // end of myOnClick
- 
-}   // end of function myOnClick for addeventlistener for choicesList
+    
+    if (curQuest == questions.length-1) {
+      nextBtn.disabled = true;
+      recScoreBtn = false;
+    }
+    else {
+      nextBtn.textContent = "Next";
+    }
+  }   // end of onClickChoice
+
+  function btnClickInitials() {
+    initialsEl = document.querySelector("#initials");
+    hofTable.push([initialsEl.textContent, score, numCorrAnsSoFar]);
+  }
+
+// End of quiz
+// 1. get scores from storage
+function AddScore() {
+  // Stringify and set key in localStorage to todos array
+  localStorage.setItem("hofTable", JSON.stringify(hofTable));
+
+}   // End of function endOfQuiz() 
+
+function fillHOFTable() {
+  // Read scores from local storage
+  hofTable = JSON.parse(localStorage.getItem("hofTable"));
+  
+  // If todos were retrieved from localStorage, update the todos array to it
+  var j;
+  var rowid;
+  var initialsEl;
+  var colid;
+  var scoreEl;
+  if (storedScores !== null) {
+    for (var i=0; i<Math.min(storedScores.length, 5); i++) {
+      rowid = "#initials" + i;
+      initialsEl = document.querySelector(rowid);
+      initialsEl.textContent = storeScores[i,0];
+      colid = "#score" + i;
+      scoreEl = document.querySelector(colid);
+      scoreEl.textContent = storeScores[i,0];
+    }
+  }
+}
+
+function saveScore() {
+
+}
+
+}   // end of function onClickChoice for addeventlistener for choicesList
